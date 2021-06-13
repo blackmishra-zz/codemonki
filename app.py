27f922c5da8749584b729864e90aa381657745a2
Shashank <mishra.shashank9598@gmail.com>
2021-06-13 10:23:49 +0530

    App init with views and nav links

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    # return "Hello, World!"
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    # return "Hello, World!"
+    return render_template("about.html")
+
+
+@app.route("/profile")
+def profile():
+    # return "Hello, World!"
+    return render_template("profile.html")
+
+
+@app.route("/orders")
+def orders():
+    # return "Hello, World!"
+    return render_template("orders.html")
+
+
+@app.route("/products")
+def products():
+    # return "Hello, World!"
+    return render_template("products.html")
+
+
+@app.route("/login")
+def login():
+    # return "Hello, World!"
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    # return "Hello, World!"
+    return render_template("signup.html")
+
+
+# app Starter code
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
